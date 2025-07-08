d28a43815a33e068244eb1eac47b2b5ab07aa93e
Aniruddh Rao <aniruddhrao03@gmail.com>
2025-07-08 16:16:09 -0400

    part 2 yaw angle and velocity

--- part-2.m
+++ part-2.m
@@ -1,18 +1,58 @@
-m = 1400;               % mass (kg)
-a = 1.14;               % front axle distance (m)
-b = 1.33;               % rear axle distance (m)
-Cf = 25000;             % front cornering stiffness (N/rad)
-Cr = 21000;             % rear cornering stiffness (N/rad)
-Iz = 2420;              % yaw inertia (kg.m^2)
-u = 75 * 1000 / 3600;   % longitudinal velocity (m/s)
-deltat = 0.1;           % step steer input (rad)
+clear; clc; close all;
 
-% State-space matrices
-A = [-(Cf+Cr)/(m*u), (-a*Cf + b*Cr)/(m*u) - u;
-    - (a*Cf - b*Cr)/(Iz*u), -(a^2*Cf + b^2*Cr)/(Iz*u)];
+%% Vehicle Parameters
+m = 1400;
+a = 1.14;
+b = 1.33;
+Cf = 25000;
+Cr = 21000;
+Iz = 2420;
+u = 75 * 1000 / 3600;   % m/s
+delta = 0.1;
 
-B = [Cf/m;
-     a*Cf/Iz];
+%A and B matrices 
+A = [-(Cf + Cr)/(m*u), (-a*Cf + b*Cr)/(m*u) - u; -(a*Cf - b*Cr)/(Iz*u), -(a^2*Cf + b^2*Cr)/(Iz*u)];
+B = [Cf/m; a*Cf/Iz];
 
- dt = 0.1
- t_vec =  0:dt:15
+%% Time setup
+h = 0.01;
+t = 0:h:5;
+N = length(t);
+
+% Initialize vectors: [v_y; psi]
+x_euler = zeros(2, N);
+x_rk4   = zeros(2, N);
+
+%Euler Method
+for n = 1:N-1
+    x_euler(:,n+1) = x_euler(:,n) + h * (A * x_euler(:,n) + B * delta);
+end
+
+%RK4 Method 
+for n = 1:N-1
+    k1 = A * x_rk4(:,n) + B * delta;
+    k2 = A * (x_rk4(:,n) + 0.5*h*k1) + B * delta;
+    k3 = A * (x_rk4(:,n) + 0.5*h*k2) + B * delta;
+    k4 = A * (x_rk4(:,n) + h*k3) + B * delta;
+    x_rk4(:,n+1) = x_rk4(:,n) + (h/6)*(k1 + 2*k2 + 2*k3 + k4);
+end
+
+%Plot Lateral Velocity
+figure;
+plot(t, x_euler(1,:), 'r', 'LineWidth', 1, 'DisplayName', 'Euler v_y'); %Euler Plot
+hold on;
+plot(t, x_rk4(1,:), 'b', 'LineWidth', 1, 'DisplayName', 'RK4 v_y'); %RK4 Plot
+xlabel('Time (s)');
+ylabel('Lateral Velocity (m/s)');
+title('Lateral Velocity Response');
+legend; grid on;
+
+% Plot Yaw Angle
+figure;
+plot(t, x_euler(2,:), 'r', 'LineWidth', 1.5, 'DisplayName', 'Euler yaw angle');
+hold on;
+plot(t, x_rk4(2,:), 'b', 'LineWidth', 1.5, 'DisplayName', 'RK4 yaw angle');
+xlabel('Time (s)');
+ylabel('Yaw Angle (rad)');
+title('Yaw Angle Response');
+legend; grid on;
